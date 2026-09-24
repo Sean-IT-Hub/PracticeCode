84cd596eaeb95ef0ba825f82bd8eb1f36e59cf50
Sean-IT-Hub <bizzh86@gmail.com>
2026-09-24 20:58:14 +0800

    Refactor school_user creation and quota settings
    Removed user drop statement and added user quota.

--- script/orcale.sql
+++ script/orcale.sql
@@ -1,9 +1,14 @@
 alter session set container = ORCLPDB;
 
-Drop user school_user cascade;
-
 CREATE USER school_user
 IDENTIFIED BY School123;
 
 GRANT CREATE SESSION, CREATE TABLE
 TO school_user;
+
+SELECT username
+FROM dba_users
+WHERE username = 'SCHOOL_USER';
+
+ALTER USER school_user
+QUOTA UNLIMITED ON users;
